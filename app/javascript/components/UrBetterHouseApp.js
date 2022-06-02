cd0e3d59cbd96bd8fa30e527cf9da260e9e1cb53
Shon Wang <munouzin@gmail.com>
2022-06-02 19:22:13 +0800

    generate UrBetterHouseApp react component

---
+++ app/javascript/components/UrBetterHouseApp.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Button } from "@mui/material";
+class UrBetterHouseApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Button variant="contained">Hello, Bear!</Button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default UrBetterHouseApp
