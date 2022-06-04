72adecaed17ef8c82721cb785e85e33e67617f14
Shon Wang <munouzin@gmail.com>
2022-06-04 14:31:36 +0800

    create model Favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :token_authenticatable
+  has_many :favorites
 end
