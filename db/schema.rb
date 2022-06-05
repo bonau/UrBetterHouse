7b5729b2c56c7a7cd7864b8d81e678f27cffaf15
Shon Wang <munouzin@gmail.com>
2022-06-05 21:12:15 +0800

    add role to user & db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_053202) do
+ActiveRecord::Schema.define(version: 2022_06_05_131155) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_06_05_053202) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "authentication_token"
     t.datetime "authentication_token_created_at"
+    t.string "role"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
